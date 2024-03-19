3534219affbd0d93ff92202809e9e90a6ec5aaf0
Wenhao Dong <wenhaodong9031@gmail.com>
2024-03-19 11:46:11 -0400

    add Npgsql connection code

--- Music4U/Program.cs
+++ Music4U/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotenv.net;
+using Npgsql;
+
+DotEnv.Load();
+
+var connectionString = "Host=127.0.0.1;Port=5432;Database=p320_25";
+
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+await using var command = dataSource.CreateCommand("SELECT * FROM song");
+await using var reader = await command.ExecuteReaderAsync();
+
+while (await reader.ReadAsync())
+{
+    Console.WriteLine(reader.GetString(0));
+}
