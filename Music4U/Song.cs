a842e7c3f30ce6a967d23dbaad4692cf81a8d734
Aerie <chris222f@gmail.com>
2024-03-21 12:14:31 -0400

    started implementing song class

---
+++ Music4U/Song.cs
@@ -0,0 +1,13 @@
+
+public class Song
+{
+    public string Title { get; }
+    public TimeOnly Time { get; }
+    public DateOnly ReleaseDate { get; }
+    public int SongId { get; }
+    
+
+
+
+
+}
