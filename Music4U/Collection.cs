c7a616a6fcdfc8c5ba1ad662018a7bb6fb539b1a
Wenhao Dong <wenhaodong9031@gmail.com>
2024-03-26 13:43:54 -0400

    fix collection class errors

--- Music4U/Collection.cs
+++ Music4U/Collection.cs
@@ -1,22 +1,6 @@
-using System.Security.Cryptography;
-using System.Text;
-using Npgsql;
-
 public class Collection
 {
-    public int Id { get; }
-    public User Useremail { get; }
-    public string Name { get; }
-    
-
-    private Collection(string email, string username, string firstName, string lastName, DateOnly creationDate, DateTime lastAccess)
-    {
-        Email = email;
-        Username = username;
-        FirstName = firstName;
-        LastName = lastName;
-        CreationDate = creationDate;
-        LastAccess = lastAccess;
-    }
-
+    public readonly int Id;
+    public readonly string UserEmail;
+    public readonly string Name;
 }
