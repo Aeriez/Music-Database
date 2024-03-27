076ba242299d96f561c24711570ff3e3407a749a
Wenhao Dong <wenhaodong9031@gmail.com>
2024-03-26 21:10:39 -0400

    add Collection constructor

--- Music4U/Collection.cs
+++ Music4U/Collection.cs
@@ -3,4 +3,11 @@ public class Collection
     public readonly int Id;
     public readonly string UserEmail;
     public readonly string Name;
+
+    private Collection(int id, string userEmail, string name)
+    {
+        Id = id;
+        UserEmail = userEmail;
+        Name = name;
+    }
 }
