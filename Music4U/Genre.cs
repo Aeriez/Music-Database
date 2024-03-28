a81de1fb0543b7f8e7cfe17ce2be91c9893a0f8d
Tristan Loutan <tll1477@g.rit.edu>
2024-03-28 11:31:38 -0400

    Added constructors to album, artist, and genre

--- Music4U/Genre.cs
+++ Music4U/Genre.cs
@@ -2,4 +2,10 @@ public class Genre
 {
     public string Name { get; }
     public int GenreId { get; }
+
+    private Genre(string name, int genreId)
+    {
+        Name = name;
+        GenreId = genreId;
+    }
 }
