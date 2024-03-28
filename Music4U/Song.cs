08733d47e9126b381539f397c5724c78d388daa7
Aerie <chris222f@gmail.com>
2024-03-28 11:38:27 -0400

    Song constructor

--- Music4U/Song.cs
+++ Music4U/Song.cs
@@ -5,6 +5,17 @@ public class Song
     public TimeOnly Time { get; }
     public DateOnly ReleaseDate { get; }
     public int SongId { get; }
+
+
+    private Song(string title, TimeOnly time, DateOnly releaseDate, int songId) 
+    {
+        this.Title = title;
+        this.Time = time;
+        this.ReleaseDate = releaseDate;
+        this.SongId = songId;
+    }
+
+    
     
 
 
