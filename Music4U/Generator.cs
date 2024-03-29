110d2293566fb0644069aad710b299e0e6935a14
Wenhao Dong <wenhaodong9031@gmail.com>
2024-03-29 14:37:17 -0400

    add random population methods

--- Music4U/Generator.cs
+++ Music4U/Generator.cs
@@ -1,9 +1,17 @@
+using Npgsql;
+
+
 /// <summary>
 /// Class for generating random data.
 /// </summary>
 public class Generator
 {
     private static readonly Random random = new();
+
+    public static readonly string[] Genres = [
+        "Rock", "Pop", "Jazz", "EDM", "Rap", "Country", "Folk", "Blues",
+        "R&B", "Latin", "Punk", "Hip-Hop", "Metal",
+    ];
 
     private static readonly string[] adjectives = [
         "Eternal", "Broken", "Golden", "Frozen", "Silent", "Lonely", "Shining",
@@ -185,5 +193,208 @@ public class Generator
         }
 
         return (album, songs);
+    }
+
+    public static DateTime RandomDate(DateTime start, DateTime end)
+    {
+        var range = (end - start).Days;
+        return start.AddDays(random.Next(range));
+    }
+
+    public static int[] PopulateArtists(NpgsqlConnection conn, int count)
+    {
+        var ids = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            var name = Generator.RandomArtist();
+            const string sql = @"
+                INSERT INTO artist (name) VALUES (@name)
+                RETURNING artist_id
+            ";
+
+            using var command = new NpgsqlCommand(sql, conn)
+            {
+                Parameters = { new("name", name) }
+            };
+
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+            ids[i] = (int)command.ExecuteScalar();
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+        }
+
+        return ids;
+    }
+
+    public static void PopulateAlbum(NpgsqlConnection conn, int artistId, int[] artistIds)
+    {
+        var (albumName, songs) = RandomAlbumWithSongs();
+        var albumDate = RandomDate(new DateTime(1980, 1, 1), DateTime.Now);
+
+        const string albumSql = @"
+            INSERT INTO album (name, release_date)
+            VALUES (@name, @release_date)
+            RETURNING album_id
+        ";
+
+        // Create the album
+        int albumId;
+        using (var command = new NpgsqlCommand(albumSql, conn))
+        {
+            command.Parameters.Add(new("name", albumName));
+            command.Parameters.Add(new("release_date", albumDate));
+
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+            albumId = (int)command.ExecuteScalar();
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+        }
+
+        const string albumArtistSql = @"
+            INSERT INTO artist_creates_album (artist_id, album_id)
+            VALUES (@artist_id, @album_id)
+        ";
+
+        // Add the artist to the album
+        using (var command = new NpgsqlCommand(albumArtistSql, conn))
+        {
+            command.Parameters.Add(new("artist_id", artistId));
+            command.Parameters.Add(new("album_id", albumId));
+
+            command.ExecuteNonQuery();
+        }
+
+        // Select a random set of possible genres
+        var genereCount = random.Next(3) + 1;
+        HashSet<int> genreIds = new();
+        for (int i = 0; i < genereCount; i++)
+        {
+            genreIds.Add(random.Next(Genres.Length) + 1);
+        }
+        var genrePool = genreIds.ToArray();
+
+        // Add the songs to the album
+        for (int i = 0; i < songs.Length; i++)
+        {
+            var song = songs[i];
+
+            const string songSql = @"
+                INSERT INTO song (title, time, release_date)
+                VALUES (@title, @time, @release_date)
+                RETURNING song_id
+            ";
+
+            var durationMinutes = random.Next(2, 6);
+            var durationSeconds = random.Next(60);
+            var duration = new TimeSpan(0, durationMinutes, durationSeconds);
+
+            var releaseDateOffset = random.Next(100);
+            var releaseDate = albumDate - TimeSpan.FromDays(releaseDateOffset);
+
+            // Create the song
+            int songId;
+            using (var command = new NpgsqlCommand(songSql, conn))
+            {
+                command.Parameters.Add(new("title", song));
+                command.Parameters.Add(new("time", duration));
+                command.Parameters.Add(new("release_date", releaseDate));
+
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+                songId = (int)command.ExecuteScalar();
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+            }
+
+            const string albumContainsSongSql = @"
+                INSERT INTO song_appears_in_album (album_id, song_id, track_num)
+                VALUES (@album_id, @song_id, @track_num)
+            ";
+
+            // Add the song to the album
+            using (var command = new NpgsqlCommand(albumContainsSongSql, conn))
+            {
+                command.Parameters.Add(new("album_id", albumId));
+                command.Parameters.Add(new("song_id", songId));
+                command.Parameters.Add(new("track_num", i + 1));
+
+                command.ExecuteNonQuery();
+            }
+
+            int? featuring = null;
+            // random chance of featuring another artist
+            if (random.Next(10) == 0)
+            {
+                featuring = artistIds[random.Next(artistIds.Length)];
+                if (featuring == artistId)
+                {
+                    featuring = null;
+                }
+            }
+
+            const string songArtistSql = @"
+                INSERT INTO artist_creates_song (artist_id, song_id)
+                VALUES (@artist_id, @song_id)
+            ";
+
+            // Add the artist to the song
+            using (var command = new NpgsqlCommand(songArtistSql, conn))
+            {
+                command.Parameters.Add(new("artist_id", artistId));
+                command.Parameters.Add(new("song_id", songId));
+
+                command.ExecuteNonQuery();
+            }
+
+            // Add the featuring artist to the song
+            if (featuring != null)
+            {
+                using var command = new NpgsqlCommand(songArtistSql, conn)
+                {
+                    Parameters = {
+                        new("artist_id", featuring),
+                        new("song_id", songId),
+                    }
+                };
+
+                command.ExecuteNonQuery();
+            }
+
+            // Add genres to the song
+            var genreNum = random.Next(1, genrePool.Length);
+            for (int j = 0; j < genreNum; j++)
+            {
+                const string songGenreSql = @"
+                    INSERT INTO song_categorized_by_genre (song_id, genre_id)
+                    VALUES (@song_id, @genre_id)
+                ";
+
+                using var command = new NpgsqlCommand(songGenreSql, conn)
+                {
+                    Parameters = {
+                        new("song_id", songId),
+                        new("genre_id", genrePool[j]),
+                    }
+                };
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Add genres to the album
+        foreach (var genre in genrePool)
+        {
+            const string albumGenreSql = @"
+                INSERT INTO album_categorized_by_genre (album_id, genre_id)
+                VALUES (@album_id, @genre_id)
+            ";
+
+            using var command = new NpgsqlCommand(albumGenreSql, conn)
+            {
+                Parameters = {
+                    new("album_id", albumId),
+                    new("genre_id", genre),
+                }
+            };
+
+            command.ExecuteNonQuery();
+        }
     }
 }
